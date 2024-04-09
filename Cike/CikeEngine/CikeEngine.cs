531f7c88861af7ecd667b4cc1caaa57a05a386a6
Danylo Treshchov <danylo.treshchov@mycit.ie>
2024-04-09 23:46:37 +0100

    Created Vector2D, started working on the game window.

---
+++ Cike/CikeEngine/CikeEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cike.CikeEngine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class CikeEngine
+    {
+        private Vector2D screenSize = new Vector2D(512, 512);
+        private string title;
+        private Canvas window = null;
+
+        public CikeEngine(Vector2D screenSize, string title)
+        {
+            this.screenSize = screenSize;
+            this.title = title;
+
+            this.window = new Canvas();
+            window.Size = new Size((int)screenSize.X, (int)screenSize.Y);
+            window.Text = title;
+
+            Application.Run(window);
+        }
+    }
+}
---
+++ Cike/CikeEngine/Vector2D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cike.CikeEngine
+{
+    public class Vector2D
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector2D()
+        {
+            this.X = Zero().X;
+            this.Y = Zero().Y;
+        }
+
+        public Vector2D(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static Vector2D Zero()
+        {
+            return new Vector2D(0, 0);
+        }
+
+        //Operators:
+
+        public static Vector2D operator -(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector2D operator +(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector2D operator *(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.X * v2.X, v1.Y * v2.Y);
+        }
+
+        public static Vector2D operator *(Vector2D v, float scalar)
+        {
+            return new Vector2D(v.X * scalar, v.Y * scalar);
+        }
+
+        public static Vector2D operator /(Vector2D v, float scalar)
+        {
+            return new Vector2D(v.X / scalar, v.Y / scalar);
+        }
+
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+
+        public Vector2D Normaize()
+        {
+            if(Magnitude() == 0)
+            {
+                return this;
+            }
+            return this / Magnitude();
+        }
+    }
+}
